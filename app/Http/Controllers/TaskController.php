4d8839a1025d0f271c0a3ca65481390cf90b3585
3A417118 <s3A417118@student.ncut.edu.tw>
2018-11-19 16:17:39 +0800

    Create Function_Add new mission

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,11 +1,31 @@
 <?php
  namespace App\Http\Controllers;
+ 
+ use App\Http\Requests;
+
  use Illuminate\Http\Request;
+ use App\Http\Controllers\Controller;
+
  class TaskController extends Controller
 {
     //
     public function __construct()
     {
         $this->middleware('auth');
+    
+	public function index(Request $request)
+    {
+        return view('tasks.index');
+    }
+    
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+         $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+         return redirect('/tasks');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,11 @@
 |
 */
 
+Route::auth();
+
 Route::get('/', function () {
-     return view('home');
+         return view('welcome');
+
 });
 
 Route::get('/home', function () {
